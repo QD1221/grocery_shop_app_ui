92dd1b2f3a6e532f471456ee57132ff19dd78a5b
QD1221 <zxcvbnm12122000@gmail.com>
2021-07-07 15:59:19 +0700

    4th commit

--- lib/ui/grocery_home_page.dart
+++ lib/ui/grocery_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_shop_app_ui/widget/grocery_category_widget.dart';
 import 'package:grocery_shop_app_ui/widget/grocery_home_widget.dart';
 
 class GroceryHomePage extends StatefulWidget {
@@ -14,86 +15,78 @@ class _GroceryHomePageState extends State<GroceryHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xff08150D),
-      body: ValueListenableBuilder(
-          valueListenable: _bottomTabIdx,
-          builder: (BuildContext context, int value, Widget child) {
-            switch (value) {
-              case 0:
-                return SafeArea(
-                  child: GroceryHomeWidget()
-                );
-              case 1:
-                return SafeArea(
-                  child: Container(
-                    child: Center(
-                      child: Text(
-                        'Page 2',
-                        style: TextStyle(color: Colors.white),
+        resizeToAvoidBottomInset: false,
+        backgroundColor: const Color(0xff08150D),
+        body: ValueListenableBuilder(
+            valueListenable: _bottomTabIdx,
+            builder: (BuildContext context, int value, Widget child) {
+              switch (value) {
+                case 0:
+                  return SafeArea(child: GroceryHomeWidget());
+                case 1:
+                  return SafeArea(child: GroceryCategoryWidget());
+                case 2:
+                  return SafeArea(
+                    child: Container(
+                      child: Center(
+                        child: Text(
+                          'Page 3',
+                          style: TextStyle(color: Colors.white),
+                        ),
                       ),
                     ),
-                  ),
-                );
-              case 2:
-                return SafeArea(
-                  child: Container(
-                    child: Center(
-                      child: Text(
-                        'Page 3',
-                        style: TextStyle(color: Colors.white),
+                  );
+                case 3:
+                  return SafeArea(
+                    child: Container(
+                      child: Center(
+                        child: Text(
+                          'Page 4',
+                          style: TextStyle(color: Colors.white),
+                        ),
                       ),
                     ),
-                  ),
-                );
-              case 3:
-                return SafeArea(
-                  child: Container(
-                    child: Center(
-                      child: Text(
-                        'Page 4',
-                        style: TextStyle(color: Colors.white),
+                  );
+                default:
+                  return SafeArea(
+                    child: Container(
+                      child: Center(
+                        child: Text(
+                          'Page ?',
+                          style: TextStyle(color: Colors.white),
+                        ),
                       ),
                     ),
-                  ),
-                );
-              default:
-                return SafeArea(
-                  child: Container(
-                    child: Center(
-                      child: Text(
-                        'Page ?',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
-                );
-                break;
-            }
-          }),
-      bottomNavigationBar: ValueListenableBuilder(valueListenable: _bottomTabIdx, builder: (context, value, child){
-        return BottomNavigationBar(
-          onTap: (idx) {
-            _bottomTabIdx.value = idx;
-          },
-          currentIndex: value,
-          backgroundColor: const Color(0xff08150D),
-          type: BottomNavigationBarType.fixed,
-          selectedItemColor: const Color(0xff52C66C),
-          unselectedItemColor: Colors.white,
-          selectedLabelStyle: const TextStyle(fontSize: 12),
-          unselectedLabelStyle: const TextStyle(fontSize: 12),
-          items: [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.home_outlined), label: 'Home'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.dashboard_outlined), label: 'Categories'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.favorite_border), label: 'Favorite'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.shopping_cart_outlined), label: 'Cart'),
-          ],
-        );
-      })
-    );
+                  );
+                  break;
+              }
+            }),
+        bottomNavigationBar: ValueListenableBuilder(
+            valueListenable: _bottomTabIdx,
+            builder: (context, value, child) {
+              return BottomNavigationBar(
+                onTap: (idx) {
+                  _bottomTabIdx.value = idx;
+                },
+                currentIndex: value,
+                backgroundColor: const Color(0xff08150D),
+                type: BottomNavigationBarType.fixed,
+                selectedItemColor: const Color(0xff52C66C),
+                unselectedItemColor: Colors.white,
+                selectedLabelStyle: const TextStyle(fontSize: 12),
+                unselectedLabelStyle: const TextStyle(fontSize: 12),
+                items: [
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.home_outlined), label: 'Home'),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.dashboard_outlined),
+                      label: 'Categories'),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.favorite_border), label: 'Favorite'),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.shopping_cart_outlined), label: 'Cart'),
+                ],
+              );
+            }));
   }
 }
